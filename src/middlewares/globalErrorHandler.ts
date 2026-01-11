10e1e64d63195086a9ec089e4bb20ebbb28eb3e1
Mahfuj-HSTU <rmahfujur16@gmail.com>
2026-01-11 18:35:17 +0600

    add unknown request error to the global error handler

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -28,8 +28,28 @@ function errorHandler(
       statusCode = 400
       errormessage = 'Foreign key constraint failed.'
     }
+  } else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+    statusCode = 500
+    errormessage = 'An unknown error occurred with the database client.'
+  } else if (err instanceof Prisma.PrismaClientRustPanicError) {
+    statusCode = 500
+    errormessage = 'A panic occurred in the Prisma Client Rust engine.'
+  } else if (err instanceof Prisma.PrismaClientInitializationError) {
+    if (err.errorCode === 'P1000') {
+      statusCode = 401
+      errormessage = 'Authentication failed against the database.'
+    } else if (err.errorCode === 'P1001') {
+      statusCode = 503
+      errormessage = 'The database server is not available.'
+    } else if (err.errorCode === 'P1002') {
+      statusCode = 503
+      errormessage = 'The database timed out while trying to connect.'
+    } else {
+      statusCode = 500
+      errormessage =
+        'An error occurred during the initialization of the Prisma Client.'
+    }
   }
-
   res.status(statusCode)
   res.json({
     success: false,
